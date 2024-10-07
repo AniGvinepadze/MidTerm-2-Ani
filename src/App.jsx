a2b86d0cb401814a159c612660766d90da360bb5
Ani <a.gvin3@gmail.com>
2024-10-07 22:13:47 +0400

    feature/practice1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [result,setResult] = useState([]);
+
+  const handleSubmit =(e) =>{
+    e.preventDefault()
+    const newTask = {
+      name:value
+    }
+    setResult((prev)=>[...prev ,newTask])
+  }
+
+  
+
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">Midterm 2</h1>
+      <div>
+        <form onSubmit={handleSubmit} >
+        <div>
+          <input
+            type="text"
+            onChange={(e) => setValue(e.target.value)}
+            className="w-[200px] h-[40px]"
+          />
+          <button type="submit">click me </button>
+        </div>
+        </form>
+        <ul>
+          {result.map((task)=>(
+            <li key = {task.name}>
+              {task.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
